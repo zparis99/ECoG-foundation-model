45620bf741ef7f2ed0b896cd3508ed4bd62e32f5
Souvik Mandal <mandalsouvik76@gmail.com>
2024-01-08 01:58:07 +0530

    add contrastive loss

--- fMRI-MAE/utils.py
+++ fMRI-MAE/utils.py
@@ -132,3 +132,22 @@ def count_params(model):
     trainable = sum(p.numel() for p in model.parameters() if p.requires_grad)
     print("param counts:\n{:,} total\n{:,} trainable".format(total, trainable))
     return trainable
+
+
+def contrastive_loss(
+    cls_token1: torch.Tensor, cls_token2: torch.Tensor, temperature: torch.Tensor
+):
+    feat1 = cls_token1 / cls_token1.norm(dim=1, keepdim=True)
+    feat2 = cls_token2 / cls_token2.norm(dim=1, keepdim=True)
+
+    cosine_sim = feat1 @ feat2.T
+    logit_scale = temperature.exp()  # log scale, learned during training
+    feat1 = cosine_sim * logit_scale
+    feat2 = feat1.T
+
+    labels = torch.arange(feat1.shape[0]).to(feat1.device)
+    loss = (
+        torch.nn.functional.cross_entropy(feat1, labels)
+        + torch.nn.functional.cross_entropy(feat2, labels)
+    ) / 2
+    return loss
