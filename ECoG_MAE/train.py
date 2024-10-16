b5960ac97ac236577c6ac070e0c26a29a99bd9f6
Zach Paris <zparis99@gmail.com>
2024-10-15 18:51:20 -0700

    remove current logging setup

--- ECoG_MAE/train.py
+++ ECoG_MAE/train.py
@@ -54,23 +54,10 @@ def train_model(
     """
     model_config = config.video_mae_task_config.vit_config
 
-    ### class token config ###
-    use_cls_token = model_config.use_cls_token
-
-    ### Loss Config ###
-    use_contrastive_loss = config.video_mae_task_config.use_contrastive_loss
-    constrastive_loss_weight = 1.0
-    use_cls_token = (
-        True if use_contrastive_loss else use_cls_token
-    )  # if using contrastive loss, we need to add a class token
-
     torch.cuda.empty_cache()
     model.to(device)
 
     num_frames = config.ecog_data_config.sample_length * config.ecog_data_config.new_fs
-
-    epoch = 0
-    best_test_loss = 1e9
 
     if config.trainer_config.learning_rate == None:
         model, optimizer, train_dl, lr_scheduler = accelerator.prepare(
@@ -78,32 +65,6 @@ def train_model(
         )
 
     mse = nn.MSELoss(reduction='none')
-    if use_contrastive_loss:
-        logit_scale = nn.Parameter(torch.ones([]) * np.log(1 / 0.07))
-
-    (
-        lrs,
-        train_losses,
-        test_losses,
-        seen_train_losses,
-        seen_test_losses,
-        contrastive_losses,
-    ) = ([], [], [], [], [], [])
-
-    signal_means, signal_stds = [], []
-    train_corr = pd.DataFrame()
-    seen_train_corr = pd.DataFrame()
-    unseen_train_corr = pd.DataFrame()
-    seen_train_avg_corr = pd.DataFrame()
-    unseen_train_avg_corr = pd.DataFrame()
-    test_corr = pd.DataFrame()
-    seen_test_corr = pd.DataFrame()
-    unseen_test_corr = pd.DataFrame()
-    seen_test_avg_corr = pd.DataFrame()
-    unseen_test_avg_corr = pd.DataFrame()
-    model_recon = pd.DataFrame()
-    trains = []
-    tests = []
 
     progress_bar = tqdm(
         range(epoch, config.trainer_config.num_epochs),
@@ -118,16 +79,6 @@ def train_model(
                 optimizer.zero_grad()
 
                 signal = batch.to(device)
-
-                trains.append(
-                    {
-                        "train batch ": str(train_i),
-                        "num_samples": str(signal.shape),
-                    }
-                )
-
-                signal_means.append(torch.mean(signal).detach().to("cpu").item())
-                signal_stds.append(torch.std(signal).detach().to("cpu").item())
 
                 if config.ecog_data_config.norm == "batch":
                     signal = normalize(signal)
@@ -214,47 +165,6 @@ def train_model(
                     decoder_padding_mask,
                 )
 
-                # get correlation between original and reconstructed signal
-                new_train_corr, new_seen_train_corr, new_unseen_train_corr = (
-                    get_correlation(
-                        config.ecog_data_config.bands,
-                        signal,
-                        full_recon_signal,
-                        tube_mask,
-                        epoch,
-                        train_i,
-                    )
-                )
-                train_corr = pd.concat([train_corr, new_train_corr])
-
-                seen_train_corr = pd.concat([seen_train_corr, new_seen_train_corr])
-
-                unseen_train_corr = pd.concat(
-                    [unseen_train_corr, new_unseen_train_corr]
-                )
-
-                new_seen_train_avg_corr = get_correlation_across_elecs(
-                    config.ecog_data_config.bands,
-                    seen_target_signal,
-                    seen_recon_signal,
-                    epoch,
-                    train_i,
-                )
-                seen_train_avg_corr = pd.concat(
-                    [seen_train_avg_corr, new_seen_train_avg_corr]
-                )
-
-                new_unseen_train_avg_corr = get_correlation_across_elecs(
-                    config.ecog_data_config.bands,
-                    unseen_target_signal,
-                    unseen_recon_signal,
-                    epoch,
-                    train_i,
-                )
-                unseen_train_avg_corr = pd.concat(
-                    [unseen_train_avg_corr, new_unseen_train_avg_corr]
-                )
-
                 # calculate loss
                 if config.trainer_config.loss == "patch":
                     loss = mse(recon_output, recon_target).nanmean()
@@ -288,24 +198,10 @@ def train_model(
                 accelerator.backward(loss)
                 optimizer.step()
 
-                train_losses.append(loss.item())
-                seen_train_losses.append(seen_loss.item())
-                lrs.append(optimizer.param_groups[0]["lr"])
-
             model.eval()
             for test_i, batch in enumerate(test_dl):
 
                 signal = batch.to(device)
-
-                tests.append(
-                    {
-                        "test batch ": str(test_i),
-                        "num_samples": str(signal.shape),
-                    }
-                )
-
-                signal_means.append(torch.mean(signal).detach().to("cpu").item())
-                signal_stds.append(torch.std(signal).detach().to("cpu").item())
 
                 if config.ecog_data_config.norm == "batch":
                     signal = normalize(signal)
@@ -391,44 +287,6 @@ def train_model(
                     decoder_padding_mask,
                 )
 
-                # get correlation between original and reconstructed signal
-                new_test_corr, new_seen_test_corr, new_unseen_test_corr = (
-                    get_correlation(
-                        config.ecog_data_config.bands,
-                        signal,
-                        full_recon_signal,
-                        tube_mask,
-                        epoch,
-                        test_i,
-                    )
-                )
-                test_corr = pd.concat([test_corr, new_test_corr])
-
-                seen_test_corr = pd.concat([seen_test_corr, new_seen_test_corr])
-
-                unseen_test_corr = pd.concat([unseen_test_corr, new_unseen_test_corr])
-
-                new_seen_test_avg_corr = get_correlation_across_elecs(
-                    config.ecog_data_config.bands,
-                    seen_target_signal,
-                    seen_recon_signal,
-                    epoch,
-                    test_i,
-                )
-                seen_test_avg_corr = pd.concat(
-                    [seen_test_avg_corr, new_seen_test_avg_corr]
-                )
-
-                new_unseen_test_avg_corr = get_correlation_across_elecs(
-                    config.ecog_data_config.bands,
-                    unseen_target_signal,
-                    unseen_recon_signal,
-                    epoch,
-                    test_i,
-                )
-                unseen_test_avg_corr = pd.concat(
-                    [unseen_test_avg_corr, new_unseen_test_avg_corr]
-                )
 
                 # calculate loss
                 if config.trainer_config.loss == "patch":
@@ -457,9 +315,6 @@ def train_model(
                         seen_target_signal[:, 4, :, :],
                     ).nanmean()
 
-                test_losses.append(loss.item())
-                seen_test_losses.append(seen_loss.item())
-
                 # save original and reconstructed signal for plotting (for highgamma)
                 if test_i in [0, 5, 10, 15, 20]:
 
@@ -487,194 +342,13 @@ def train_model(
 
             print("Epoch " + str(epoch) + " done. Time elapsed: " + str(end - start))
 
-            logs = {
-                "train/loss": np.mean(train_losses[-(train_i + 1) :]),
-                "test/loss": np.mean(test_losses[-(test_i + 1) :]),
-            }
-            progress_bar.set_postfix(**logs)
-
         # save model checkpoints
         checkpoint = {
             "epoch": epoch,
             "model": model,
             "optimizer": optimizer.state_dict(),
             "lr_scheduler": lr_scheduler.state_dict(),
-            "train_losses": train_losses,
-            "seen_train_losses": seen_train_losses,
-            "test_losses": test_losses,
-            "seen_test_losses": seen_test_losses,
             "experiment_config": config,
         }
-
-        dir = os.getcwd() + f"/checkpoints/"
-        if not os.path.exists(dir):
-            os.makedirs(dir)
-
-        torch.save(checkpoint, dir + f"{config.job_name}_checkpoint.pth")
-
-        # save correlations
-        dir = os.getcwd() + f"/results/correlation/"
-        if not os.path.exists(dir):
-            os.makedirs(dir)
-
-        train_corr.to_csv(
-            dir + f"{config.job_name}_train_corr.csv",
-            index=False,
-        )
-
-        seen_train_corr.to_csv(
-            dir + f"{config.job_name}_seen_train_corr.csv",
-            index=False,
-        )
-
-        unseen_train_corr.to_csv(
-            dir + f"{config.job_name}_unseen_train_corr.csv",
-            index=False,
-        )
-
-        seen_train_avg_corr.to_csv(
-            dir + f"{config.job_name}_seen_train_avg_corr.csv",
-            index=False,
-        )
-
-        unseen_train_avg_corr.to_csv(
-            dir + f"{config.job_name}_unseen_train_avg_corr.csv",
-            index=False,
-        )
-
-        test_corr.to_csv(
-            dir + f"{config.job_name}_test_corr.csv",
-            index=False,
-        )
-
-        seen_test_corr.to_csv(
-            dir + f"{config.job_name}_seen_test_corr.csv",
-            index=False,
-        )
-
-        unseen_test_corr.to_csv(
-            dir + f"{config.job_name}_unseen_test_corr.csv",
-            index=False,
-        )
-
-        seen_test_avg_corr.to_csv(
-            dir + f"{config.job_name}_seen_test_avg_corr.csv",
-            index=False,
-        )
-
-        unseen_test_avg_corr.to_csv(
-            dir + f"{config.job_name}_unseen_test_avg_corr.csv",
-            index=False,
-        )
-
-        # save train and test sample information
-        train_samples = pd.DataFrame(trains)
-        test_samples = pd.DataFrame(tests)
-
-        dir = os.getcwd() + f"/results/test_loader/"
-        if not os.path.exists(dir):
-            os.makedirs(dir)
-
-        train_samples.to_csv(
-            dir + f"{config.job_name}_train_samples.csv",
-            index=False,
-        )
-
-        test_samples.to_csv(
-            dir + f"{config.job_name}_test_samples.csv",
-            index=False,
-        )
-
-        # plot results
-        # Just closing plots isn't enough to free up the RAM used by matplotlib. Instead what we need to do to return that memory
-        # is to plot in a subprocess which will be killed at completion. When that process is killed
-        # we actually get the memory back.
-        # See https://stackoverflow.com/questions/28516828/memory-leaks-using-matplotlib?rq=4 for more details.
-        def _plot_summary(
-            config: VideoMAEExperimentConfig,
-            train_losses,
-            test_losses,
-            use_contrastive_loss,
-            contrastive_losses,
-            signal_means,
-            signal_stds,
-            train_corr,
-            seen_train_corr,
-            unseen_train_corr,
-            seen_train_avg_corr,
-            unseen_train_avg_corr,
-            test_corr,
-            seen_test_corr,
-            unseen_test_corr,
-            seen_test_avg_corr,
-            unseen_test_avg_corr,
-            model_recon,
-        ):
-            plot_losses(
-                config.job_name,
-                train_losses,
-                seen_train_losses,
-                test_losses,
-                seen_test_losses,
-            )
-            if use_contrastive_loss:
-                plot_contrastive_loss(config.job_name, contrastive_losses)
-
-            plot_signal_stats(config.job_name, signal_means, signal_stds)
-
-            plot_correlation(config.job_name, train_corr, "train_correlation")
-            plot_correlation(config.job_name, seen_train_corr, "seen_train_correlation")
-            plot_correlation(
-                config.job_name, unseen_train_corr, "unseen_train_correlation"
-            )
-            plot_correlation(
-                config.job_name, seen_train_avg_corr, "seen_train_avg_correlation"
-            )
-            plot_correlation(
-                config.job_name, unseen_train_avg_corr, "unseen_train_avg_correlation"
-            )
-            plot_correlation(config.job_name, test_corr, "test_correlation")
-            plot_correlation(config.job_name, seen_test_corr, "seen_test_correlation")
-            plot_correlation(
-                config.job_name, unseen_test_corr, "unseen_test_correlation"
-            )
-            plot_correlation(
-                config.job_name, seen_test_avg_corr, "seen_test_avg_correlation"
-            )
-            plot_correlation(
-                config.job_name, unseen_test_avg_corr, "unseen_test_avg_correlation"
-            )
-            plot_recon_signals(config.job_name, config.trainer_config.num_epochs, model_recon)
-
-            plt.close("all")
-
-        proc = mp.Process(
-            target=_plot_summary,
-            args=(
-                config,
-                train_losses,
-                test_losses,
-                use_contrastive_loss,
-                contrastive_losses,
-                signal_means,
-                signal_stds,
-                train_corr,
-                seen_train_corr,
-                unseen_train_corr,
-                seen_train_avg_corr,
-                unseen_train_avg_corr,
-                test_corr,
-                seen_test_corr,
-                unseen_test_corr,
-                seen_test_avg_corr,
-                unseen_test_avg_corr,
-                model_recon,
-            ),
-        )
-        proc.start()
-        # wait until proc terminates.
-        proc.join()
-
-        print("Plotting " + str(epoch) + " done.")
 
     return model
