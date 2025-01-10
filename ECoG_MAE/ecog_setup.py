ae2c6420c04de95a39aefb1d8e885e4ad64726ad
Zach Paris <zparis99@gmail.com>
2025-01-09 23:08:52 -0800

    break up ipynb file into 3 smaller ones

--- ECoG_MAE/ecog_setup.py
+++ ECoG_MAE/ecog_setup.py
@@ -72,9 +72,8 @@ def model_setup(config: VideoMAEExperimentConfig, device, num_train_samples):
     model_config = config.video_mae_task_config.vit_config
 
     num_frames = int(
-        config.ecog_data_config.sample_length * config.ecog_data_config.new_fs / 1000
+        config.ecog_data_config.sample_length * config.ecog_data_config.new_fs
     )
-
     frame_patch_size = model_config.frame_patch_size
     num_patches = int(  # Defining the number of patches
         constants.GRID_SIZE**2
@@ -93,28 +92,7 @@ def model_setup(config: VideoMAEExperimentConfig, device, num_train_samples):
     print("num_encoder_patches", num_encoder_patches)
     print("num_decoder_patches", num_decoder_patches)
 
-    model = MaskedAutoencoderViT(
-        img_size=constants.GRID_SIZE,
-        patch_size=model_config.patch_size,
-        in_chans=len(config.ecog_data_config.bands),
-        embed_dim=model_config.dim,
-        depth=model_config.depth,
-        num_heads=model_config.num_heads,
-        decoder_embed_dim=model_config.decoder_embed_dim,
-        decoder_depth=model_config.decoder_depth,
-        decoder_num_heads=model_config.decoder_num_heads,
-        mlp_ratio=model_config.mlp_ratio,
-        num_frames=num_frames,
-        t_patch_size=model_config.frame_patch_size,
-        no_qkv_bias=model_config.no_qkv_bias,
-        sep_pos_embed=model_config.sep_pos_embed,
-        trunc_init=model_config.trunc_init,
-        cls_embed=model_config.use_cls_token,
-        # TODO: Make this configurable.
-        pred_t_dim=num_frames,
-        img_mask=None,
-        pct_masks_to_decode=config.video_mae_task_config.pct_masks_to_decode,
-    )
+    model = create_model(config)
     utils.count_params(model)
 
     no_decay = ["bias", "LayerNorm.bias", "LayerNorm.weight"]
@@ -153,3 +131,33 @@ def model_setup(config: VideoMAEExperimentConfig, device, num_train_samples):
     print("\nDone with model preparations!")
 
     return model, optimizer, lr_scheduler, num_patches
+
+
+def create_model(config: VideoMAEExperimentConfig):
+    model_config = config.video_mae_task_config.vit_config
+    num_frames = int(
+        config.ecog_data_config.sample_length * config.ecog_data_config.new_fs
+    )
+    model = MaskedAutoencoderViT(
+        img_size=constants.GRID_SIZE,
+        patch_size=model_config.patch_size,
+        in_chans=len(config.ecog_data_config.bands),
+        embed_dim=model_config.dim,
+        depth=model_config.depth,
+        num_heads=model_config.num_heads,
+        decoder_embed_dim=model_config.decoder_embed_dim,
+        decoder_depth=model_config.decoder_depth,
+        decoder_num_heads=model_config.decoder_num_heads,
+        mlp_ratio=model_config.mlp_ratio,
+        num_frames=num_frames,
+        t_patch_size=model_config.frame_patch_size,
+        no_qkv_bias=model_config.no_qkv_bias,
+        sep_pos_embed=model_config.sep_pos_embed,
+        trunc_init=model_config.trunc_init,
+        cls_embed=model_config.use_cls_token,
+        # TODO: Make this configurable.
+        pred_t_dim=num_frames,
+        img_mask=None,
+        pct_masks_to_decode=config.video_mae_task_config.pct_masks_to_decode,
+    )
+    return model
